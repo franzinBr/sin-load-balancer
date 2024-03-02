a492120cffa806b0ccfd65acf41bd01a5191efd4
franzinBr <alan.franzin2011@gmail.com>
2024-03-02 16:33:24 -0400

    fix server methods

--- internal/server/server.go
+++ internal/server/server.go
@@ -23,31 +23,30 @@ func (s *Server) CheckHealthy(endpoint string, sucessStatusCode int, interval ti
 		url := s.URL.String() + endpoint
 
 		res, err := http.Get(url)
-		s.Healthy = err != nil && res.StatusCode == sucessStatusCode
+
+		s.Healthy = err == nil && res != nil && res.StatusCode == sucessStatusCode
 	}
 
 }
 
-func (s *Server) Proxy() *httputil.ReverseProxy {
-
+func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	s.Mutex.Lock()
 	s.Connections++
 	s.Mutex.Unlock()
 
-	rProxy := httputil.NewSingleHostReverseProxy(s.URL)
+	proxy := httputil.NewSingleHostReverseProxy(s.URL)
+	proxy.ServeHTTP(w, r)
 
 	s.Mutex.Lock()
 	s.Connections--
 	s.Mutex.Unlock()
-
-	return rProxy
 }
 
 func BuildServers(config *config.Config) []*Server {
 	var servers []*Server
 	for _, serverUrl := range config.Server.URLs {
 		u, _ := url.Parse(serverUrl)
-		servers = append(servers, &Server{URL: u, Healthy: true})
+		servers = append(servers, &Server{URL: u})
 	}
 
 	return servers
