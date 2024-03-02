89eb570264ff7365443a78dfdba5de4f3d1c1c18
franzinBr <alan.franzin2011@gmail.com>
2024-03-02 15:30:15 -0400

    create method to check server healthy

--- internal/server/server.go
+++ internal/server/server.go
@@ -17,6 +17,17 @@ type Server struct {
 	Mutex       sync.Mutex
 }
 
+func (s *Server) CheckHealthy(endpoint string, sucessStatusCode int, interval time.Duration) {
+
+	for range time.Tick(time.Duration(interval)) {
+		url := s.URL.String() + endpoint
+
+		res, err := http.Get(url)
+		s.Healthy = err != nil && res.StatusCode == sucessStatusCode
+	}
+
+}
+
 func BuildServers(config *config.Config) []*Server {
 	var servers []*Server
 	for _, serverUrl := range config.Server.URLs {
